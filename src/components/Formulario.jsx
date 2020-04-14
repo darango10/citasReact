80112695912cf0da584f0b38132b69550b2dff60
Daniel <arangomonsalve.daniel@gmail.com>
2020-04-14 13:11:14 -0500

    Leer lo que el usuario escribe en los inputs

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,83 @@
+import React, {Fragment, useState} from 'react';
+
+const Formulario = () => {
+
+    //Crear State de Citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    //Funcion que se ejecuta cada que el usuario escribe un input
+    const actualizarState = (e) => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    //Extraer los valores
+    const {mascota, propietario, fecha, hora, sintomas} = cita
+
+
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name='mascota'
+                    className='u-full-width'
+                    placeholder={'Nombre Mascota'}
+                    onChange={actualizarState}
+                    value={mascota}
+                />
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name='propietario'
+                    className='u-full-width'
+                    placeholder={'Nombre Dueño de la mascota'}
+                    onChange={actualizarState}
+                    value={propietario}
+                />
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name='fecha'
+                    className='u-full-width'
+                    onChange={actualizarState}
+                    value={fecha}
+                />
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name='hora'
+                    className='u-full-width'
+                    onChange={actualizarState}
+                    value={hora}
+                />
+                <label>Sintomas</label>
+                <textarea
+                    className='u-full-width'
+                    name={'sintomas'}
+                    onChange={actualizarState}
+                    value={sintomas}
+                />
+
+                <button
+                    type='submit'
+                    className='u-full-width button-primary'
+                >Agregar Cita
+                </button>
+
+            </form>
+        </Fragment>
+    );
+};
+
+export default Formulario;
